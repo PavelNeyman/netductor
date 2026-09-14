c1800404147d0cc535ee018480e9412a3f6b531c
PavelNeyman <hunter.trojan@gmail.com>
2026-09-14 10:36:13 +0300

    WL defaults dual-hop: internal/vpn/clientcfg_test.go

--- internal/vpn/clientcfg_test.go
+++ internal/vpn/clientcfg_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSingBoxClientJSONMinimal(t *testing.T) {
 	e := ClientEndpoints{Name: "t", UUID: "11111111-1111-1111-1111-111111111111",
-		CoreHost: "1.2.3.4", CorePort: 443, CoreSNI: "ya.ru", CorePBK: "pbk", CoreSID: "ab"}
+		CoreHost: "1.2.3.4", CorePort: 443, CoreSNI: DefaultRealitySNI, CorePBK: "pbk", CoreSID: "ab"}
 	b, err := SingBoxClientJSON(e)
 	if err != nil || len(b) < 20 {
 		t.Fatal(err, string(b))
