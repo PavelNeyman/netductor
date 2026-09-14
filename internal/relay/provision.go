1aef01ac66b7c0d6764d3fb0aee61d9fd2ef89e3
PavelNeyman <hunter.trojan@gmail.com>
2026-09-15 00:22:17 +0300

    provision: auto post + SNI api.vk.me (internal/relay/provision.go)

--- internal/relay/provision.go
+++ internal/relay/provision.go
@@ -17,7 +17,7 @@ type ProvisionIn struct {
 	Port     int    // SSH port, default 22
 	User     string // default root
 	Password string
-	SNI      string // Reality SNI on relay, default ya.ru
+	SNI      string // Reality SNI on relay; empty → ResolveRelaySNI / api.vk.me
 }
 
 // ProvisionResult summarizes remote setup.
