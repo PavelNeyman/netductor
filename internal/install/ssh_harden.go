eb0439949d5830627c3eaea59b68d0e6e5a416e2
PavelNeyman <hunter.trojan@gmail.com>
2026-09-15 11:51:20 +0300

    ssh harden: first drop-in + neutralize password yes

--- internal/install/ssh_harden.go
+++ internal/install/ssh_harden.go
@@ -42,10 +42,32 @@ func EnsureSSHKeyAndHarden() error {
 	_ = os.Chmod(priv, 0o600)
 
 	_ = os.MkdirAll("/etc/ssh/sshd_config.d", 0o755)
-	drop := "/etc/ssh/sshd_config.d/99-netductor-harden.conf"
+	// Lexically first so we win over cloud-init 00password.conf (sshd: first obtained value wins)
+	drop := "/etc/ssh/sshd_config.d/00-netductor-harden.conf"
 	body := "PasswordAuthentication no\nKbdInteractiveAuthentication no\nChallengeResponseAuthentication no\nPermitRootLogin prohibit-password\nPubkeyAuthentication yes\n"
 	if err := os.WriteFile(drop, []byte(body), 0o644); err != nil {
 		return err
+	}
+	// Neutralize other drop-ins that force password yes
+	entries, _ := os.ReadDir("/etc/ssh/sshd_config.d")
+	for _, e := range entries {
+		name := e.Name()
+		if name == "00-netductor-harden.conf" {
+			continue
+		}
+		path := filepath.Join("/etc/ssh/sshd_config.d", name)
+		b, err := os.ReadFile(path)
+		if err != nil {
+			continue
+		}
+		s := string(b)
+		if strings.Contains(strings.ToLower(s), "passwordauthentication") && strings.Contains(strings.ToLower(s), "yes") {
+			s2 := strings.ReplaceAll(s, "PasswordAuthentication yes", "PasswordAuthentication no")
+			s2 = strings.ReplaceAll(s2, "PasswordAuthentication Yes", "PasswordAuthentication no")
+			if s2 != s {
+				_ = os.WriteFile(path, []byte(s2), 0o644)
+			}
+		}
 	}
 	_ = exec.Command("sed", "-i", "s/^#\\?PasswordAuthentication.*/PasswordAuthentication no/", "/etc/ssh/sshd_config").Run()
 	_ = exec.Command("sed", "-i", "s/^#\\?PermitRootLogin.*/PermitRootLogin prohibit-password/", "/etc/ssh/sshd_config").Run()
