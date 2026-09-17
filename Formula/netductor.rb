7672a9fb0ccefa46f9af7b9204daf22d5e490106
PavelNeyman <hunter.trojan@gmail.com>
2026-09-17 21:23:11 +0300

    Formula: 0.7.10-dev

--- Formula/netductor.rb
+++ Formula/netductor.rb
@@ -1,14 +1,14 @@
 class Netductor < Formula
   desc "Netductor control plane CLI (VPN fleet, edge, TUI)"
   homepage "https://github.com/PavelNeyman/netductor"
-  version "0.7.9-dev"
+  version "0.7.10-dev"
   license "MIT"
   url "https://github.com/PavelNeyman/netductor/releases/download/v0.7.0-dev/netductor-darwin-arm64"
-  sha256 "a4c590c4663d4d4bff6dee766fd09453d12254c034ef56fbf4b1e77e3579f970"
+  sha256 "4b37d5d2ddc6bf1de566140729e2c060cf4447d74f765c2777c5bf0846425581"
   on_linux do
     on_intel do
       url "https://github.com/PavelNeyman/netductor/releases/download/v0.7.0-dev/netductor-linux-amd64"
-      sha256 "2504ba348a2656e26b097f449398e6af3b3fe099507600b5336761821d31678d"
+      sha256 "774b9ceab3eb9e7a7fec084b70666e032a082182e885f9a030e7ddefaf2d3bad"
     end
   end
   def install
